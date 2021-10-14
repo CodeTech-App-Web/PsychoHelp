dfe6468d0bb7625c3f5328c3f625c6154e108a3e
Jose Tarazona <joset2804@hotmail.com>
2021-10-14 17:30:44 -0500

    Create login-pacient

--- src/router/index.js
+++ src/router/index.js
@@ -34,12 +34,9 @@ const routes = [
     component: () => import('../components/errorpage')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/loginpacient',
+    name: 'Login-Pacient',
+    component: () => import('../components/login-pacient')
   }
 ]
 
