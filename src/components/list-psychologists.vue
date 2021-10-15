c039576c219bf7336731b62c292b7d3cd092e003
Debie García de la Cruz Melo <debiegarcia@hotmail.com>
2021-10-15 17:48:59 -0500

    Update list-psychologists.vue

--- src/components/list-psychologists.vue
+++ src/components/list-psychologists.vue
@@ -3,10 +3,41 @@
     <v-row>
       <v-col cols="2">
         <v-sheet rounded="lg">
-          <h>Filters</h>
+          <v-container>
+            <v-layout row wrap>
+              <v-flex xs12 sm12>
+                <v-card>
+                  <v-card-title>Filtros</v-card-title>
+                  <v-card-actions>
+                  <v-text-field v-model="search" label="Search Name" outlined v-on:input="getPsychologistByName()"></v-text-field>
+                  </v-card-actions>
+                  <v-divider></v-divider>
+                  <v-card-title>Genero</v-card-title>
+                  <v-card-text>
+                    <v-radio-group v-model="genre">
+                      <v-radio label="Masculino" value="Masculino" v-on:click="getPsychologistsByFilter(genre, sessionType)"></v-radio>
+                      <v-radio label="Femenino" value="Femenino" v-on:click="getPsychologistsByFilter(genre, sessionType)"></v-radio>
+                    </v-radio-group>
+                  </v-card-text>
+                  <v-divider></v-divider>
+                  <v-card-title>Tipo de Sesión</v-card-title>
+                  <v-card-text>
+                  <v-radio-group v-model="sessionType">
+                    <v-radio label="Individual" value="Individual" v-on:click="getPsychologistsByFilter(genre, sessionType)"></v-radio>
+                    <v-radio label="De Pareja" value="Pareja" v-on:click="getPsychologistsByFilter(genre, sessionType)"></v-radio>
+                  </v-radio-group>
+                  </v-card-text>
+                  <v-divider></v-divider>
+                  <v-card-actions>
+                  <v-btn class="mr-4" @click="clear()" block>Clear</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
         </v-sheet>
       </v-col>
-
+      <!--LISTA DE PSICOLOGOS-->
       <v-col>
         <v-sheet rounded="lg">
       <v-container>
@@ -44,11 +75,48 @@
       </v-col>
     </v-row>
     <template>
-      <!--Dialog-->
-      <v-dialog v-model="dialog" width="800" v-if="selectedPsychologist!=null">
+      <!--DIALOG INFO PSICOLOGO SELECCIONADO-->
+      <v-dialog v-model="dialog" width="600" v-if="selectedPsychologist!=null" persistent>
         <v-card>
-          <v-card-title>{{selectedPsychologist.name}}</v-card-title>
-          <v-card-text>{{ selectedPsychologist.about }}</v-card-text>
+          <v-col align="center">
+            <v-avatar width="100" height="100">
+              <v-img :src="selectedPsychologist.img"></v-img>
+            </v-avatar>
+          </v-col>
+          <v-card-title class="justify-center">{{ selectedPsychologist.name }}</v-card-title>
+          <v-card-subtitle class="text-center">CMP: 215863</v-card-subtitle>
+          <v-card-text class="text-justify">{{ selectedPsychologist.about }}</v-card-text>
+          <v-container>
+            <v-layout>
+              <v-flex>
+                <v-card>
+                  <v-card-title class="blue--text text--darken-2">Áreas de Atención</v-card-title>
+                  <v-card-text>
+                  <v-flex v-for="specialization in selectedPsychologist.specialization" :key="specialization">
+                    {{specialization}}
+                  </v-flex>
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <v-container>
+            <v-layout>
+              <v-flex>
+                <v-card>
+                  <v-card-title class="blue--text text--darken-2">Formación Académica</v-card-title>
+                  <v-card-text>
+                    <v-flex v-for="formation in selectedPsychologist.formation" :key="formation">
+                      {{formation}}
+                    </v-flex>
+                  </v-card-text>
+                </v-card>
+                <v-flex class="mt-2 text-end">
+                <v-btn @click.stop="dialog=false">Salir</v-btn>
+                </v-flex>
+              </v-flex>
+            </v-layout>
+          </v-container>
         </v-card>
       </v-dialog>
       <!--Fin del Dialog-->
@@ -58,13 +126,20 @@
 
 <script>
 import PsychologistsApiService from "../../src/core/services/psychologists-api.service"
+import { validationMixin } from 'vuelidate'
 export default {
   name: "list-psychologists",
+  mixins: [validationMixin],
   data: ()=> ({
     psychologists: [],
     dialog: false,
     selectedPsychologist: null,
+    selected: [],
+    genre: null,
+    sessionType: null,
+    search: ""
   }),
+
   created() {
     this.retrievePsychologists();
     this.dialog = false;
@@ -86,6 +161,57 @@ export default {
       console.log('psychologistDialog psychologist:', psychologist);
       this.selectedPsychologist = psychologist;
       this.dialog = true;
+    },
+
+    getPsychologistsByFilter(genre, sessionType){
+      if(genre!=null && sessionType===null) {
+        PsychologistsApiService.findByGenre(genre)
+            .then(response => {
+              this.psychologists = response.data;
+              console.log(response.data);
+            })
+            .catch(e => {
+              console.log(e);
+            });
+      }
+      if(genre===null && sessionType!=null){
+        PsychologistsApiService.findBySessionType(sessionType)
+            .then(response => {
+              this.psychologists = response.data;
+              console.log(response.data);
+            })
+            .catch(e => {
+              console.log(e);
+            });
+      }
+      if(genre!=null && sessionType!=null){
+        PsychologistsApiService.findByGenreAndSessionType(genre,sessionType)
+            .then(response => {
+              this.psychologists = response.data;
+              console.log(response.data);
+            })
+            .catch(e => {
+              console.log(e);
+            });
+      }
+    },
+
+    getPsychologistByName(){
+        PsychologistsApiService.findByName(this.search)
+            .then(response => {
+              this.psychologists = response.data;
+              console.log(response.data);
+            })
+            .catch(e => {
+              console.log(e);
+            });
+    },
+
+
+    clear () {
+      this.retrievePsychologists();
+      this.genre=null;
+      this.sessionType=null;
     }
 
   }
