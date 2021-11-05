5b9753eea95e1f8c8057fc2c7e2e7d86a19e892d
Debie García de la Cruz Melo <debiegarcia@hotmail.com>
2021-11-04 19:26:05 -0500

    Update patient-dashboard.vue

--- src/components/patients/patient-dashboard.vue
+++ src/components/patients/patient-dashboard.vue
@@ -1,17 +1,19 @@
 <template>
   <div style=";width: 100%">
-    <div class="ma-2 mb-4 font-weight-medium" style="font-size:3rem">Tus pacientes</div>
+    <div class="ma-2 mb-4 font-weight-medium" style="font-size:1.5rem">Tus Pacientes</div>
     <v-sheet color="white" elevation="1" height="100%" rounded width="100%">
       <v-row align="center" justify="center">
         <v-col class="d-flex flex-wrap" >
-          <v-card class="mx-auto mb-10 pb-3 justify-center" max-width="400" v-for="(patientDB,i) in patientDBs"
+          <v-card class="mx-auto mb-10 pb-3 justify-center" width="280" v-for="(patientDB,i) in patientDBs"
                   :key="i">
-            <v-avatar class="ma-4" size="250">
-              <img :src="patientDB.img"></v-avatar>
-            <v-card-title class="justify-center">{{ patientDB.lastname }} {{ patientDB.firstname }}</v-card-title>
-            <v-card-subtitle class="text-center font-weight-medium" style="font-size:1rem">{{ patientDB.date }}
+            <v-col align="center">
+              <v-avatar class="mt-4" width="100" height="100">
+                <img :src="patientDB.img"></v-avatar>
+            </v-col>
+            <v-card-title class="justify-center font-weight-bold text-lg-body-1">{{ patientDB.lastname }} {{ patientDB.firstname }}</v-card-title>
+            <v-card-subtitle class="text-center" style="font-size:1rem">{{ patientDB.date }}
             </v-card-subtitle>
-            <v-card-text class="text-center font-weight-medium">{{ patientDB.email }}</v-card-text>
+            <v-card-text class="text-center" style="color: black">{{ patientDB.email }}</v-card-text>
           </v-card>
         </v-col>
       </v-row>
