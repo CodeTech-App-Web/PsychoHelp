8c2a2941a67f78a1095984ad8140b62eb8e6203a
Jose Tarazona <joset2804@hotmail.com>
2021-11-20 01:46:20 -0500

    Created internationalization

--- src/core/services/http-common.js
+++ src/core/services/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://localhost:5001/api/v1",
+    baseURL: "https://psychohelp-api.azurewebsites.net/api/v1",
     headers: {"Content-type": "application/json"}
 });
